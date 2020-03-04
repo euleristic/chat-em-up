d465031fdb55849dcee4460a8c446cc840472498
VictorYoung22 <victor-young@live.dk>
2020-03-04 16:49:24 +0100

    Why do I have to commit

--- Project Ladybug/Project Ladybug/Assets/Scripts/Movement1.cs
+++ Project Ladybug/Project Ladybug/Assets/Scripts/Movement1.cs
@@ -7,42 +7,79 @@ public class Movement1 : MonoBehaviour
     public float speed;
     private Vector2 input;
     private Rigidbody rb;
+    private bool invincible;
+    [SerializeField] private SpriteRenderer sr;
+    [SerializeField] private float hitFadeIn;
+    [SerializeField] private float hitFadeOut;
     [SerializeField] private float snappiness;
-    public float gravity;
     [SerializeField] private float rotationAmount;
     [SerializeField] private float rotationSpeed;
     [SerializeField] private float airRes;
+    public float gravity;
 
     [SerializeField] private int hp;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        invincible = false;
     }
 
     
     void Update()
     {
-        input.x = Input.GetAxis("Horizontal");
-        input.y = Input.GetAxis("Vertical");
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
 
-        rb.velocity += new Vector3(input.x * snappiness, input.y * snappiness, 0f);
-        if (rb.velocity.magnitude > speed)
-            rb.velocity = rb.velocity.normalized * speed;
-        if (input.x == 0f && input.y == 0f)
+        if (input.x != 0f || input.y != 0f)
+            rb.velocity = Vector3.Lerp(rb.velocity, new Vector3(input.x * speed, input.y * speed, 0f), snappiness);
+        else if (input.x == 0f && input.y == 0f)
             rb.velocity /= airRes;
+        /*if (rb.velocity.magnitude > speed)
+            rb.velocity = rb.velocity.normalized * speed;*/
 
         float rotation = 0f;
 
         if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0.01f)
             rotation = rotationAmount * -Mathf.Sign(Input.GetAxisRaw("Horizontal"));
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0f,0f, rotation), rotationSpeed * Time.deltaTime);
+
+
+        print(sr.color);
+        //hitflash
+        if (invincible)
+        {
+            sr.color = Color.Lerp(sr.color, Color.white, hitFadeIn);
+            if ((Color.white - sr.color).a < 0.1)
+            {
+                sr.color = Color.white;
+                invincible = false;
+            }
+        }
+        else if (sr.color != Color.clear)
+        {
+            sr.color = Color.Lerp(sr.color, Color.clear, hitFadeOut);
+            if (!invincible && (sr.color - Color.clear).a < 0.1)
+            {
+                sr.color = Color.clear;
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("EnemyBullet") || other.CompareTag("AliveEnemy"))
+        if ((other.CompareTag("EnemyBullet") || other.CompareTag("AliveEnemy")) && !invincible)
+        {
             hp--;
+            invincible = true;
+        }
         if (hp <= 0)
             SceneScript.LoseState();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("EnemyBullet") || other.CompareTag("AliveEnemy") && invincible)
+            sr.color = Color.Lerp(sr.color, Color.white, hitFadeIn);
+
     }
 }
